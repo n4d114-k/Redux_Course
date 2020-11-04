ce366950ea9707510d1e6456f0605d7fe4719564
n4d114-k <n4d114.k@gmail.com>
2020-11-04 14:56:23 +0200

    add reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,6 @@
+function comments(state = []) {
+  console.log(state, action);
+  return state;
+}
+
+export default comments;
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,6 @@
+function posts(state = []) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
